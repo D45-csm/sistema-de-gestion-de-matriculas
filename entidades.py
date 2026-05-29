555d54a590f06ef916c135c9af18e96fdd33eda8
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-05-29 11:50:51 -0500

    creacion de archivo json y edicion de entidades

--- entidades.py
+++ entidades.py
@@ -1,26 +1,27 @@
-estudiantes = [
-    {
-    "id_estudiante":1,
-    "nombre":"Daniela",
-    "carrera":"ADSO"
-    }
-]
+datos_sistema= {"estudiantes": [
+                    {
+                    "id_estudiante":1,
+                    "nombre":"Daniela",
+                    "carrera":"ADSO"
+                    }
+                ],
 
-cursos = [
-    {
-    "id_curso":101,
-    "nombre_curso":"Bases de Datos",
-    "creditos": 2
-    }
-]
+                "cursos": [
+                    {
+                    "id_curso":101,
+                    "nombre_curso":"Bases de Datos",
+                    "creditos": 2
+                    }
+                ],
 
-matriculas = [
-    {
-    "id_matricula":1,
-    "id_estudiante": 1,
-    "id_curso":[101],
-    "periodo_academico":"2026-2"
-    }
-]
+                "matriculas": [
+                    {
+                    "id_matricula":1,
+                    "id_estudiante": 1,
+                    "id_curso":[101],
+                    "periodo_academico":"2026-2"
+                    }
+                ]
+                }
 
 
