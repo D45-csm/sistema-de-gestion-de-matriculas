cba63e9fdea696a3492d2c2a50a8e741eff78e6e
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-06-01 22:22:23 -0500

    funciones para guardar y caargar los datos

---
+++ guardar_cargar_datos.py
@@ -0,0 +1,25 @@
+import json
+from rich.console import Console 
+console = Console()
+
+# ---GUARDADO DE DATOS ---
+def guardar_datos(archivo_json, datos): # se debe poner un archivo json y los datos a guardar en formato de diccionario
+    try:
+        with open(archivo_json, "w", encoding="utf-8") as archivo:
+            json.dump(datos, archivo, indent=4)
+        console.print("[green]✔ Datos guardados correctamente[/green]")
+    except Exception:
+        console.print("[red]✖ Ocurrio un error al guardar los datos[/red]")
+
+# ---CARGAR DATOS ---
+def cargar_datos(archivo_json):#ingresar el archivo json al cual se desea cargar
+    try:
+        with open(archivo_json, "r", encoding="utf-8") as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        console.print("[yellow]⚠ Archivo no encontrado[/yellow]")
+        return {"estudiantes": [], "cursos": [], "matriculas": []}  #debuelve un diccionario anidado vacio
+
+#guardar_datos("Entidades/matriculas.json", matriculas) ejemplo de como se debe introducir parametros para guardar datos
+#cargar_datos("Entidades/matriculas.json") # ejemplo de como se debe introducir parametros para cargar datos
+#cargar datos no imprime nada
