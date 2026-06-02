8fd74a074141c80483a905f3b2d13aa6e001dbcd
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-06-01 22:21:08 -0500

    archivo para las entidades principales

---
+++ entidades_principales.py
@@ -0,0 +1,42 @@
+estudiantes=[
+            {
+            "id_estudiante":1,
+            "nombre":"Daniela",
+            "carrera":"ADSO",
+            "correo":"daniela@example.com",
+            "telefono":"1234567890"
+            },
+            {
+            "id_estudiante":2,
+            "nombre":"Daniel",
+            "carrera":"topografia",
+            "correo":"acos79.0.98dan1lec@gmail.com",
+            "telefono":"3118689862"
+            }
+            ]
+cursos=  [
+        {
+        "id_curso":101,
+        "nombre_curso":"Bases de Datos",
+        "creditos": 2
+        },
+        {
+        "id_curso":105,
+        "nombre_curso":"JavaScript",
+        "creditos": 2
+        },
+        {
+        "id_curso":107,
+        "nombre_curso":"Logica de programacion",
+        "creditos": 2
+        }
+        ]
+
+matriculas= [
+            {
+            "id_matricula":1,
+            "id_estudiante": 1,
+            "id_curso":[101, 107],
+            "periodo_academico":"2026-2"
+            }
+            ]
